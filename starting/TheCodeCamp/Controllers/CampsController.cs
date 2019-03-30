15fc1af0bfc5fdba661eb5d5082b15f953a6a6c3
Jeffery Cosby <jcoz1968@gmail.com>
2019-03-29 21:28:59 -0500

    GET for collections

--- starting/TheCodeCamp/Controllers/CampsController.cs
+++ starting/TheCodeCamp/Controllers/CampsController.cs
@@ -4,15 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
+using TheCodeCamp.Data;
 
 namespace TheCodeCamp.Controllers
 {
     
     public class CampsController : ApiController
     {
-        public object Get()
+        private ICampRepository _repository;
+
+        public CampsController(ICampRepository repository)
         {
-            return new { Name = "Coz", Operation = "Teacher" };
+            _repository = repository;
+        }
+
+        public async Task<IHttpActionResult> Get()
+        {
+            try
+            {
+                var result = await _repository.GetAllCampsAsync();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                // TODO Add Logging
+                return InternalServerError(ex);
+                //$"There was an error getting your data {ex.Message}"
+            }
         }
     }
 }
